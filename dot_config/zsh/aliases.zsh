a8f2abd400fe22fb8543e4f23b28b353152f3768
Alexander Menjura <menjuraalexander@gmail.com>
2026-07-27 21:06:53 -0500

    refactor(zsh): start modular shell configuration

---
+++ dot_config/zsh/aliases.zsh
@@ -0,0 +1,18 @@
+## Aliases
+
+alias ll='exa -lbGF --git'
+alias dircolors='gdircolors'
+alias ls='ls --color=auto'
+alias k='kubecolor'
+alias kube='kubecolor'
+alias grep='grep --color=auto'
+alias du1='du -h -d 1'
+alias tf='terraform'
+
+# Git
+alias gs='git status'
+alias ga='git add'
+alias gc='git commit'
+alias gp='git push'
+alias gpl='git pull'
+alias gb='git branch'
---
+++ dot_config/zsh/exports.zsh
@@ -0,0 +1,17 @@
+export PATH="/opt/homebrew/bin:$PATH"
+export PATH="/opt/homebrew/sbin:$PATH"
+export PATH="/Users/alexandermenjura/.local/bin:$PATH"
+
+export HISTSIZE=50000
+export HISTFILESIZE=50000
+export SAVEHIST=50000
+
+export DOCKER_DEFAULT_PLATFORM=linux/amd64
+
+export EDITOR=/opt/homebrew/bin/nvim
+export VISUAL=/opt/homebrew/bin/nvim
+
+export NVM_DIR="$HOME/.nvm"
+
+export PATH="$HOME/.jenv/bin:$PATH"
+export PATH="$HOME/.npm-global/bin:$PATH"
